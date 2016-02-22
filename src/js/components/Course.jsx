b7a16e63b5947e0a2096fcc1d1a191e36f94e60a
jona <jona16@gmail.com>
2016-02-22 16:10:44 +0000

    clean up course comp and css

--- src/js/components/Course.jsx
+++ src/js/components/Course.jsx
@@ -20,7 +20,7 @@ export default class Course extends React.Component {
     super(props)
 
     this.state = {
-      data: []
+      data: {}
     }
   }
 
@@ -29,21 +29,30 @@ export default class Course extends React.Component {
   }
 
   render(){
-    let dateFormat = "MMMM Do YYYY, h:mm a"
+    if(this.state.data.id !== undefined){
+      let dateFormat = "MMMM Do YYYY, h:mm a"
 
-    return (
-      <div>
-        <div className='top-nav'>
-          <Link to="/" className="btn"><i className="fa fa-arrow-left"></i> Back to Course List</Link>
+      let startAt = this.state.data.start_at ? moment(this.state.data.start_at).format(dateFormat) : 'Not specified'
+      let endAt = this.state.data.end_at ? moment(this.state.data.end_at).format(dateFormat) : 'Not specified'
+      let createdAt = this.state.data.created_at ? moment(this.state.data.created_at).format(dateFormat) : 'Not specified'
+
+      return (
+        <div>
+          <div className='top-nav'>
+            <Link to="/" className="btn"><i className="fa fa-arrow-left"></i> Back to Course List</Link>
+          </div>
+
+          <div className='course'>
+            <h1>{this.state.data.name} - {this.state.data.code}</h1>
+            <h2><strong>Starts at</strong>: {startAt} - <strong>Ends at</strong>: {endAt}</h2>
+            <p>{this.state.data.description}</p>
+            <div><strong>Created on</strong>: {createdAt}</div>
+          </div>
         </div>
-        <div className='course'>
-          <h1>{this.state.data.name} - {this.state.data.code}</h1>
-          <h2><strong>Starts at</strong>: {this.state.data.start_at ? moment(this.state.data.start_at).format('MMMM Do YYYY, h:mm a') : 'Not specified'} - <strong>Ends at</strong>: {this.state.data.end_at ? moment(this.state.data.end_at).format('MMMM Do YYYY, h:mm a') : 'Not specified'}</h2>
-          <p>{this.state.data.description}</p>
-          <div><strong>Created on</strong>: {this.state.data.created_at ? moment(this.state.data.created_at).format('MMMM Do YYYY, h:mm a') : 'Not specified'}</div>
-        </div>
-      </div>
-    )
+      )
+    } else {
+      return <div className="loading-holder"><i className='fa fa-refresh fa-spin'></i> Loading Course</div>
+    }
   }
 
   fetchData(id) {
