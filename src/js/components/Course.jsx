945c312fbe9612255cf79bd0d851a9b99484eb03
jona <jona16@gmail.com>
2016-02-22 02:00:19 +0000

    basic routing and components

---
+++ src/js/components/Course.jsx
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+export default class Course extends React.Component {
+  render(){
+    return null
+  }
+}
---
+++ src/js/index.jsx
@@ -0,0 +1,18 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Router, Route, browserHistory, IndexRoute } from 'react-router'
+
+import CourseList from './components/CourseList'
+import Course from './components/Course'
+
+ReactDOM.render(
+  (
+    <Router history={browserHistory}>
+      <Route path="/" component={CourseList} />
+      <Route path="/courses/:name" component={Course} />
+    </Router>
+  ),
+  document.getElementById('app')
+)
